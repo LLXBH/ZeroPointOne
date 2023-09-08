6a6ecb2d41d3c6ce36342a167e34b6843a8dec46
LLXBH <llxbhkf@qq.com>
2023-09-08 23:51:27 +0800

    1. 添加自定义源（阿里源等）代理下载依赖加速；

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,15 @@
 pluginManagement {
     repositories {
+        // 自定义源（阿里源等）
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://jitpack.io") }
+        // Google 默认源
         google()
+        // Jcenter 默认源
         mavenCentral()
         gradlePluginPortal()
     }
@@ -8,7 +17,16 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // 自定义源（阿里源等）
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://jitpack.io") }
+        // Google 默认源
         google()
+        // Jcenter 默认源
         mavenCentral()
     }
 }
