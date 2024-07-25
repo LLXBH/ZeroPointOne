c9400b95a0564160575345d6575e19c42190fc3b
LLXBH <llxbhkf@qq.com>
2024-07-25 21:14:09 +0800

    improvement：接入 “规范” 接口；

--- app/src/main/java/llxbh/zeropointone/data/model/Task.kt
+++ app/src/main/java/llxbh/zeropointone/data/model/Task.kt
@@ -1,5 +1,6 @@
 package llxbh.zeropointone.data.model
 
+import android.util.Log
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
@@ -24,7 +25,7 @@ data class Task(
     var endTimes: Long = 0,
     var addTimeDay: Int = 0,
     var isDelete: Boolean = false
-): TaskLoopInterface<Task> {
+): TaskLoopInterface<Task>, TaskStandardInterface<Task> {
     override fun onLoopInspect(): Boolean {
         return when {
             // 任务未完成
@@ -60,5 +61,86 @@ data class Task(
             }
         )
         return newData
+    }
+
+    override fun onInspectContent(): Boolean {
+        try {
+            if (title.isEmpty() && content.isEmpty()) {
+                throw Exception("清单：标题和内容不能都为空。")
+            }
+            if (title.isEmpty() && content.isNotEmpty()) {
+                throw Exception("清单：标题不能为空。")
+            }
+        } catch (e: Exception) {
+            Log.e(this.javaClass.name, e.message.toString())
+            return false
+        }
+        return true
+    }
+
+    override fun onCorrectContent(): Task? {
+        this.copy().apply {
+            try {
+                // 内容不为空的的时候，把内容赋予给标题
+                if (title.isEmpty() && content.isNotEmpty()) {
+                    title = content
+                    content = ""
+                }
+                if (! onInspectContent()) {
+                    throw Exception("清单：数据修正失败。")
+                }
+            } catch (e: Exception) {
+                Log.e(this.javaClass.name, e.message.toString())
+                return null
+            }
+            return this
+        }
+    }
+
+    override fun onInspectDateTime(): Boolean {
+        try {
+            if (addTimeDay != 0 && startTimes == 0L && endTimes == 0L) {
+                throw Exception("清单：若设置了自增天数，开始时间和结束时间都不得为 0 。")
+            }
+            if (startTimes != 0L && endTimes == 0L) {
+                throw Exception("清单：开始时间和结束时间不能一边不为 0 ，一边为 0 ，如果设置同一天，则开始和结束需要相同。")
+            }
+            if (startTimes == 0L && endTimes != 0L) {
+                throw Exception("清单：开始时间和结束时间不能一边不为 0 ，一边为 0 ，如果设置同一天，则开始和结束需要相同。")
+            }
+            if (startTimes > endTimes) {
+                throw Exception("清单：结束时间不能比开始时间早。")
+            }
+        } catch (e: Exception) {
+            Log.e(this.javaClass.name, e.message.toString())
+            return false
+        }
+        return true
+    }
+
+    override fun onCorrectDateTime(): Task? {
+        this.copy().apply {
+            try {
+                if (addTimeDay != 0 && startTimes == 0L && endTimes == 0L) {
+                    addTimeDay = 0
+                }
+                if (startTimes != 0L && endTimes == 0L) {
+                    endTimes = startTimes
+                }
+                if (startTimes == 0L && endTimes != 0L) {
+                    startTimes = endTimes
+                }
+                if (startTimes > endTimes) {
+                    endTimes = startTimes
+                }
+                if (! onInspectDateTime()) {
+                    throw Exception("清单：数据修正失败。")
+                }
+            } catch (e: Exception) {
+                Log.e(this.javaClass.name, e.message.toString())
+                return null
+            }
+            return this
+        }
     }
 }
